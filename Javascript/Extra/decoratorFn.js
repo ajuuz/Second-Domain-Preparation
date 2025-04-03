5ef6d55792900bb1b8c732c196e050f9b15fbcc2
ajuuz <ajuuz07@gmail.com>
2025-04-03 13:20:13 +0530

    javascript commit 3

--- Javascript/Extra/decoratorFn.js
+++ Javascript/Extra/decoratorFn.js
@@ -4,38 +4,40 @@
 
 //- here count of calls have been founded my wrapping the add function by a decorator function . 
 
-function countCall(fn){
-    let count=0;
+// function countCall(fn){
+//     let count=0;
 
-    return function(a,b){
-        count++;
-        console.log(`function ${fn.name} called ${count} times`)
+//     return function(a,b){
+//         count++;
+//         console.log(`function ${fn.name} called ${count} times`)
 
-        return fn(a,b)
-    }
-}
+//         return fn(a,b)
+//     }
+// }
 
-function add(a,b){
-    return a+b;
-}
-
-
-const countedAdd = countCall(add);
-
-console.log(countedAdd(1,2))
-console.log(countedAdd(2,3))
-console.log(countedAdd(10,30))
-console.log(add(100,20))
+// function add(a,b){
+//     return a+b;
+// }
 
 
-// -- whithout decorator function we have to modify the original function itself
-let count=0;
-function addAndGetCount(a,b){
-    count++;
-    console.log(`call count ${count}`)
-    return a+b
-}
+// const countedAdd = countCall(add);
 
-addAndGetCount(1,2)
-addAndGetCount(1,3)
-addAndGetCount(1,4)
+// console.log(countedAdd(1,2))
+// console.log(countedAdd(2,3))
+// console.log(countedAdd(10,30))
+// console.log(add(100,20))
+
+
+// // -- whithout decorator function we have to modify the original function itself
+// let count=0;
+// function addAndGetCount(a,b){
+//     count++;
+//     console.log(`call count ${count}`)
+//     return a+b
+// }
+
+// addAndGetCount(1,2)
+// addAndGetCount(1,3)
+// addAndGetCount(1,4)
+
+
